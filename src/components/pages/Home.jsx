49fb334d8849fc8fadbb95338110fed90f12870d
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2024-02-05 10:38:39 -0500

    Se actualiza el proyecto.

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,7 +4,7 @@ function HomePage() {
   return (
     <>
       <div className="bg-gray-800 py-6 m-6 rounded-xl shadow-sm">
-        <p className="text-white text-center text-2xl">Encuentra las mejores marcar tecnologicas </p>
+        <p className="text-white text-center text-2xl">Encuentra las mejores marcas tecnologicas </p>
       </div>
       <ListaProductos />
     </>
